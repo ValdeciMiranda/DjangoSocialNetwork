61c75b31fe30b6ed10430e03e6647d3a46f9e730
Valdeci Miranda <ValdeciMiranda@outlook.com.br>
2024-04-13 13:48:31 -0300

    just add new things to it

--- socialNetwork/urls.py
+++ socialNetwork/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-
+    path("", views.index, name=""),
+    path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
+    path("feed/", views.feed, name="feed"),
+    path("logout", views.LogoutUser, name="logout"),
 ]
--- socialNetwork/views.py
+++ socialNetwork/views.py
@@ -1,7 +1,61 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import RegisterNewUserForm, UserLoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+
 def index(request):
+
     return render(request, 'index.html')
+
+
+def register(request):
+
+    form = RegisterNewUserForm()
+
+    if request.method == "POST":
+        form = RegisterNewUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        
+    context = {'registerform': form}
+
+    return render(request, 'register.html', context=context)
+
+
+def login(request):
+
+    form = UserLoginForm()
+
+    if request.method == "POST":
+        form = UserLoginForm(request, data=request.POST)
+        if form.is_valid():
+
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+
+                auth.login(request, user)
+                return redirect('feed')
+
+    context = {'loginform': form}
+
+    return render(request, 'login.html', context=context)
+
+def LogoutUser(request):
+
+    auth.logout(request)
+
+    return redirect("")
+
+@login_required(login_url='login')
+def feed(request):
+
+    return render(request, 'feed.html')
+
